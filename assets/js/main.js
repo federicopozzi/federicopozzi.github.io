c3908ee9ef9c134e3e6d8d272512b85146d78051
federicopozzi <65979192+federicopozzi@users.noreply.github.com>
2020-08-13 18:56:03 +0200

    add some stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,59 @@
+$(function() {
+$('#t-proj1').hover(function() {
+    $('#img-proj1').fadeIn();
+}, function() {
+    $('#img-proj1').fadeOut();
+});
+});
 
+$(function() {
+$('#t-proj2').hover(function() {
+    $('#img-proj2').fadeIn();
+}, function() {
+    $('#img-proj2').fadeOut();
+});
+});
+
+$(function() {
+$('#t-proj3').hover(function() {
+    $('#img-proj3').fadeIn();
+}, function() {
+    $('#img-proj3').fadeOut();
+});
+});
+
+$("#insta").hover(
+  function() {
+    $("body").addClass('b-insta');
+  },
+  function() {
+    $("body").removeClass('b-insta');
+  }
+);
+
+$("#git").hover(
+  function() {
+    $("body").addClass('b-git');
+  },
+  function() {
+    $("body").removeClass('b-git');
+  }
+);
+
+$("#telegram").hover(
+  function() {
+    $("body").addClass('b-telegram');
+  },
+  function() {
+    $("body").removeClass('b-telegram');
+  }
+);
+
+$("#twitter").hover(
+  function() {
+    $("body").addClass('b-twitter');
+  },
+  function() {
+    $("body").removeClass('b-twitter');
+  }
+);

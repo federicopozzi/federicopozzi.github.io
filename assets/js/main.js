999fb1bab85cd1f648999926a4c51fb3e5c3bc4f
federicopozzi <65979192+federicopozzi@users.noreply.github.com>
2020-08-14 17:54:53 +0200

    update homepage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,31 @@
-$(function() {
-$('#t-proj1').hover(function() {
-    $('#img-proj1').fadeIn();
-}, function() {
-    $('#img-proj1').fadeOut();
-});
-});
 
-$(function() {
-$('#t-proj2').hover(function() {
-    $('#img-proj2').fadeIn();
-}, function() {
-    $('#img-proj2').fadeOut();
-});
-});
 
-$(function() {
-$('#t-proj3').hover(function() {
-    $('#img-proj3').fadeIn();
-}, function() {
-    $('#img-proj3').fadeOut();
-});
-});
+// $(function() {
+// $('#t-proj1').hover(function() {
+//     $('#img-proj1').fadeIn();
+// }, function() {
+//     $('#img-proj1').fadeOut();
+// });
+// });
+//
+// $(function() {
+// $('#t-proj2').hover(function() {
+//     $('#img-proj2').fadeIn();
+// }, function() {
+//     $('#img-proj2').fadeOut();
+// });
+// });
+//
+// $(function() {
+// $('#t-proj3').hover(function() {
+//     $('#img-proj3').fadeIn();
+// }, function() {
+//     $('#img-proj3').fadeOut();
+// });
+// });
+
+
+
 
 $("#insta").hover(
   function() {
@@ -55,5 +60,77 @@ $("#twitter").hover(
   },
   function() {
     $("body").removeClass('b-twitter');
+  }
+);
+
+$("#vimeo").hover(
+  function() {
+    $("body").addClass('b-vimeo');
+  },
+  function() {
+    $("body").removeClass('b-vimeo');
+  }
+);
+
+$("#mail").hover(
+  function() {
+    $("body").addClass('b-mail');
+  },
+  function() {
+    $("body").removeClass('b-mail');
+  }
+);
+
+$("#insta").hover(
+  function() {
+    $("header").addClass('b-insta');
+  },
+  function() {
+    $("header").removeClass('b-insta');
+  }
+);
+
+$("#git").hover(
+  function() {
+    $("header").addClass('b-git');
+  },
+  function() {
+    $("header").removeClass('b-git');
+  }
+);
+
+$("#telegram").hover(
+  function() {
+    $("header").addClass('b-telegram');
+  },
+  function() {
+    $("header").removeClass('b-telegram');
+  }
+);
+
+$("#twitter").hover(
+  function() {
+    $("header").addClass('b-twitter');
+  },
+  function() {
+    $("header").removeClass('b-twitter');
+  }
+);
+
+$("#vimeo").hover(
+  function() {
+    $("header").addClass('b-vimeo');
+  },
+  function() {
+    $("header").removeClass('b-vimeo');
+  }
+);
+
+$("#mail").hover(
+  function() {
+    $("header").addClass('b-mail');
+  },
+  function() {
+    $("header").removeClass('b-mail');
   }
 );
